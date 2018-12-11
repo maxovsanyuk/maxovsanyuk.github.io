892d6240db01393fdfa96da8eee00a891b7e768f
Maks_Ovsianiuk <maksovsanyuk@gmail.com>
2018-12-12 00:51:33 +0200

    improveJS

--- TODO/js/data.js
+++ TODO/js/data.js
@@ -1,36 +1,45 @@
-const addItem = document.getElementById("addItem");
+
 addItem.addEventListener('click',function(){
-    let addDiv = document.createElement("div");
-    addDiv.className = "content-item__add-item";
+    const addItem = document.getElementById("addItem");
     const inputValue = document.getElementById("myInput").value;
+    
+    const addDiv = document.createElement("div");
+    addDiv.className = "content-item__add-item";
+
     const close = document.createElement("button");
     close.className = "remove-input__button";
     const createItem = document.createTextNode(inputValue);
     addDiv.appendChild(createItem);
     addDiv.appendChild(close);
+
     close.addEventListener('click', function(){
         document.getElementById("content-item").removeChild(addDiv);
     });
-    const input = document.getElementById('myInput');
-    addItem.classList.add('error-focus');
-    addItem.style.transition = ".5s"
     
-    if (inputValue === '') {
+    if (!inputValue) {
+        const input = document.getElementById('myInput');
         const error = document.getElementById('error');
+        const removeError = document.getElementById('removeError');
+
+        addItem.classList.add('button-rotate');
         error.style.display = "block";
         input.classList.add("input-error");
-        const removeError = document.getElementById('removeError');
+        
         removeError.addEventListener('click', function(){
         error.style.display = "none";
         input.classList.remove("input-error");
+        addItem.classList.remove('button-rotate');
+        addItem.classList.add('button-rotate__remove');
     });
+
     }else{
-        addItem.classList.remove("error-focus");
-        document.getElementById("content-item").appendChild(addDiv);
+        const addToContent = document.getElementById("content-item");
+        addToContent.appendChild(addDiv);
         document.getElementById("myInput").value = '';
     }
+
     addDiv.addEventListener('click', function(){
-      if(addDiv.classList.toggle('checked')){
-      }
-    }, false);
+    addDiv.classList.toggle('checked');
+    });
+    
 }); 
