d1284f4c60805d55d3aa5bff8050456d7d94669e
Maks_Ovsianiuk <maksovsanyuk@gmail.com>
2019-02-01 04:46:33 +0200

    Idop

---
+++ Idop/js/data.js
@@ -0,0 +1,7 @@
+let menuBtn = document.getElementById('menuBtn');
+let sideBar = document.getElementById('side-bar');
+
+    menuBtn.addEventListener('click',function(){
+        sideBar.classList.toggle('displey-block');
+    });
+ 
