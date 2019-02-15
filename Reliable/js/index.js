02e25059efb75a3930d8524b6c646160a571ffe4
Admin <admin@MacBook-Pro-Admin-2.local>
2019-02-15 04:18:15 +0200

    Rename Scss

--- Reliable/js/index.js
+++ Reliable/js/index.js
@@ -4,6 +4,6 @@ let menu = document.getElementById('menu');
 let hiddenMenu = document.getElementById('hidden-menu');
 menu.addEventListener('click',function(){
     menu.classList.toggle('menu-act');
-    hiddenMenu.classList.toggle('shov-hidden-menu');
+    hiddenMenu.classList.toggle('show-hidden-menu');
     document.body.classList.toggle('body');
 })
