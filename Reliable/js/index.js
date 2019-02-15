8aae433fa0347bb0e484c1a6f37de4d4e3d17673
Admin <admin@MacBook-Pro-Admin-2.local>
2019-02-15 04:04:02 +0200

    Add Reliable

---
+++ Reliable/js/index.js
@@ -0,0 +1,9 @@
+'use strict'
+
+let menu = document.getElementById('menu');
+let hiddenMenu = document.getElementById('hidden-menu');
+menu.addEventListener('click',function(){
+    menu.classList.toggle('menu-act');
+    hiddenMenu.classList.toggle('shov-hidden-menu');
+    document.body.classList.toggle('body');
+})
